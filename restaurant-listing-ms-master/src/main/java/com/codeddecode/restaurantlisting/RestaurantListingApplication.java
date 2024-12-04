32751d971c569b41e7b85eb187398074ac7d8bb9
Varun Bhardwaj <varunmzn1987@gmail.com>
2024-12-04 14:59:00 +0530

    pushing code for restauant listing

---
+++ restaurant-listing-ms-master/src/main/java/com/codeddecode/restaurantlisting/RestaurantListingApplication.java
@@ -0,0 +1,29 @@
+package com.codeddecode.restaurantlisting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class RestaurantListingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantListingApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
+
+}
